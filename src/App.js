cf433dfb2908ad359c01dd609a29ff6e64c2ef98
markus-peterson <markustpeterson@gmail.com>
2021-01-17 20:20:07 -0500

    Remove popout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Start from './views/Start'
+import { /*Paper, Grid, List, ListItem, ListItemIcon, ListItemText, */ Button, Switch} from '@material-ui/core/'
 // import ReactDOM from "react-dom";
 // import { createBrowserHistory } from "history";
 // import { Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            temp: true
+        }
+        this.changeMode = this.changeMode.bind(this)
+    }
+
+    changeMode(){
+        this.setState({temp: !this.state.temp})
+    }
+    
     render() {
+        const styles = {
+            basic: {
+                backgroundColor: this.state.temp? 'white' : '#222222',
+                color: !this.state.temp? 'white' : '#222222'
+            },
+            toggle: {
+                color: this.state.temp? '#525252' : '#9e9e9e'
+            }
+        }
         return (
-            <div className="App">
+            <div className="App" style={styles.basic}>
+                <Switch checked={this.state.temp} onChange={this.changeMode} name="temp" style={styles.toggle} inputProps={{ 'aria-label': 'secondary checkbox' }} />
+                {/* <Button onClick={this.changeMode} style={{backgroundColor: color, color: text}}>Background change</Button> */}
                 <img src={logo} className="App-logo" alt="logo" />
                 <Start />
             </div>
--- src/views/Start.jsx
+++ src/views/Start.jsx
@@ -1,35 +1,21 @@
 import React, { Component } from "react"
-import { /*Paper, Grid, List, ListItem, ListItemIcon, ListItemText, */ Button} from '@material-ui/core/'
-import Pop from '../popout/Popout'
+// import { /*Paper, Grid, List, ListItem, ListItemIcon, ListItemText, */ Button} from '@material-ui/core/'
+import Stopwatch from '../components/Stopwatch'
 // import ReactModal from 'react-modal'
 
 class Start extends Component {
     constructor(){
         super()
         this.state = {
-            popout: false
+            temp: false
         }
-        this.popout = this.popout.bind(this)
-        this.changeMode = this.changeMode.bind(this)
-    }
-
-    popout(){
-        this.setState({
-            popout: !this.state.popout
-        })
-    }
-
-    changeMode(){
-
     }
 
     render() {
         return (
             <div>
-                <Button onClick={this.changeMode}>Background change</Button>
                 <h1>Hello</h1>
-                <Button onClick={this.popout}>Popout</Button>
-                {this.state.popout && <Pop close={this.popout}/>}
+                <Stopwatch />
             </div>
         )
     }
